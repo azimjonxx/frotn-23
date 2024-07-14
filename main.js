6f0a31c5aad13734b3070eddd0ab97c7522ec679
azimjonxatamov <azimjonxatamov6@gmail.com>
2024-07-14 12:59:20 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+
+function fff() {
+   
+    let son1 = prompt("Birinchi sonni kiriting:");
+    let son2 = prompt("Ikkinchi sonni kiriting:");
+    
+   
+   
+    if (son1 > son2) {
+        alert(son1 + " eng katta son");
+    } else if (son2 > son1) {
+        alert(son2 + " eng katta son ");
+    } else {
+        alert("ikkalasi bir birga ten ");
+    }
+}
+
+
+fff();
